dd49f03ad3c284c26a93f228c17c1ed7d07760c1
Blake Goodwin <blakegoodwin@google.com>
2025-09-30 19:15:24 +0000

    Add queue_products CF
    Change-Id: I14c07d1f5c7b696218903a247ae7e6134055e226

---
+++ src/product_embeddings/queue_products/main.py
@@ -0,0 +1,85 @@
+# Copyright 2025 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Queue Products HTTP Cloud Function."""
+
+import logging
+import os
+
+import functions_framework
+from google.cloud import logging as cloud_logging
+import queue_products_lib
+
+logging_client = cloud_logging.Client()
+logging_client.setup_logging()
+
+# Global Initialization
+PROJECT_ID = os.environ.get('PROJECT_ID', 'Project ID env variable is not set.')
+DATASET_ID = os.environ.get('DATASET_ID', 'Dataset ID env variable is not set.')
+LOCATION = os.environ.get('LOCATION', 'Location env variable is not set.')
+QUEUE_ID = os.environ.get('QUEUE_ID', 'Queue ID env variable is not set.')
+CLOUD_FUNCTION_URL = os.environ.get(
+    'CLOUD_FUNCTION_URL', 'Cloud Function URL env variable is not set.'
+)
+
+product_queuer = queue_products_lib.ProductQueuer(
+    project_id=PROJECT_ID,
+    dataset_id=DATASET_ID,
+    location=LOCATION,
+    queue_id=QUEUE_ID,
+)
+
+
+@functions_framework.http
+def run(request):
+  """HTTP Cloud Function.
+
+  Args:
+      request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+
+  Returns:
+      The response text, or any set of values that can be turned into a
+      Response object using `make_response`
+      <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+  Note:
+      For more information on how Flask integrates with Cloud
+      Functions, see the `Writing HTTP functions` page.
+      <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+  """
+
+  if request.method != 'POST':
+    return 'Method Not Allowed', 405
+  if request.content_type != 'application/json':
+    return 'Unsupported Media Type', 415
+
+  request_json = request.get_json(silent=True)
+  product_limit = request_json.get('product_limit', 10)
+
+  products = product_queuer.get_new_products_from_view(
+      product_limit=product_limit
+  )
+  if products:
+    # To prevent duplicate tasks, do not push unless queue is empty.
+    if not product_queuer.is_queue_empty():
+      raise queue_products_lib.CloudTasksQueueNotEmptyError(
+          'Queue is not empty!'
+      )
+    logging.info('Found %d new products to push', len(products))
+    product_queuer.push_products(
+        products=products, cloud_function_url=CLOUD_FUNCTION_URL
+    )
+  else:
+    logging.info('No new products found, exiting...')
+  return 'OK', 200
---
+++ src/product_embeddings/queue_products/queue_products_lib.py
@@ -0,0 +1,168 @@
+# Copyright 2025 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Queue Products Module."""
+
+import dataclasses
+import json
+import logging
+
+from google.cloud import bigquery
+from google.cloud import tasks_v2
+
+try:
+  from shared import common  # pylint: disable=g-import-not-at-top
+except ImportError:
+  # This handles cases when code is not deployed using Terraform
+  from ..shared import common  # pylint: disable=g-import-not-at-top, relative-beyond-top-level
+
+Product = common.Product
+
+
+class Error(Exception):
+  """Base error class for this module."""
+
+
+class BigQueryReadError(Error):
+  """Error reading from BigQuery."""
+
+
+class CloudTasksPublishError(Error):
+  """Error publishing to Cloud Tasks."""
+
+
+class CloudTasksQueueNotEmptyError(Error):
+  """Error when queue has unfinished tasks."""
+
+
+class ProductQueuer:
+  """Product queuer class."""
+
+  def __init__(
+      self,
+      project_id: str,
+      dataset_id: str,
+      location: str,
+      queue_id: str,
+  ):
+    """Initialize instance of ProductQueuer.
+
+    Args:
+      project_id: The Google Cloud project ID.
+      dataset_id: The BigQuery dataset ID.
+      location: The Google Cloud location.
+      queue_id: The Cloud Tasks queue ID.
+    """
+    self.project_id = project_id
+    self.dataset_id = dataset_id
+    self.location = location
+    self.queue_id = queue_id
+
+    self.bigquery_client = bigquery.Client(self.project_id)
+    self.tasks_client = tasks_v2.CloudTasksClient()
+
+    self.parent_queue = self.tasks_client.queue_path(
+        self.project_id, self.location, self.queue_id
+    )
+
+  def is_queue_empty(self) -> bool:
+    """Checks if the Google Cloud Tasks queue is empty.
+
+    Returns:
+        True if the queue is empty, False otherwise.
+    """
+    request = tasks_v2.ListTasksRequest(parent=self.parent_queue)
+    response = self.tasks_client.list_tasks(request=request)
+    # Check if the queue is empty
+    return not bool(list(response.tasks))
+
+  def get_new_products_from_view(
+      self,
+      product_limit: int = 10
+  ) -> list[Product]:
+    """Retrieves set of new unprocessed products.
+
+    Args:
+      product_limit (int): Maximum number of products to retrieve
+
+    Returns:
+      a list of Product dataclasses
+    """
+    query = (
+        'SELECT'
+        '   offer_id,'
+        '   title,'
+        '   brand,'
+        '   description,'
+        '   product_type,'
+        '   google_product_category,'
+        '   age_group,'
+        '   color,'
+        '   gender,'
+        '   material,'
+        '   pattern '
+        f' FROM {self.project_id}.{self.dataset_id}.get_new_products_view'
+        f' LIMIT {product_limit}'
+    )
+    try:
+      query_job = self.bigquery_client.query(query)
+      rows = query_job.result()
+    except Exception as e:  # pylint: disable=broad-exception-caught
+      raise BigQueryReadError(e) from e
+    products = [Product(**row) for row in rows]
+    return products
+
+  def push_products(
+      self,
+      products: list[Product],
+      cloud_function_url: str,
+  ):
+    """Pushes products to Cloud Tasks.
+
+    Args:
+      products (list[Product]): A list of Product dataclasses.
+      cloud_function_url (str): The URL of the Cloud Function to call.
+
+    Raises:
+      CloudTasksPublishError: if the Cloud Tasks publish fails
+    """
+
+    task_count = 0
+
+    for product in products:
+      try:
+        # Construct the request body.
+        payload = {'product': dataclasses.asdict(product)}
+        task = tasks_v2.Task(
+            http_request=tasks_v2.HttpRequest(
+                http_method=tasks_v2.HttpMethod.POST,
+                url=cloud_function_url,
+                body=json.dumps(payload).encode('utf-8'),
+                headers={
+                    'Content-type': 'application/json',
+                },
+            )
+        )
+        # Use the client to build and send the task.
+        self.tasks_client.create_task(
+            tasks_v2.CreateTaskRequest(
+                parent=self.parent_queue,
+                task=task,
+            )
+        )
+        task_count += 1
+      except Exception as e:
+        raise CloudTasksPublishError(e) from e
+
+    logging.info('Submitted %d tasks to Cloud Tasks.', task_count)
